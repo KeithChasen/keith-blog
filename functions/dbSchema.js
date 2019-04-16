7763e30549ac4ee6b90285c742ddeea1a96ccdd5
keithchasen <keithchasen89@gmail.com>
2019-04-16 18:17:07 +0300

    added db schema file to describe DB

---
+++ functions/dbSchema.js
@@ -0,0 +1,12 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'example of body',
+            createAt: '2019-04-16T15:13:31.027Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
+
