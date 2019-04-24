e378d83016084e99574582f5122b7ee365b29f75
keithchasen <keithchasen89@gmail.com>
2019-04-24 12:57:16 +0300

    upload user photo

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -1,4 +1,4 @@
-const { db } = require('../util/admin')
+const { db, admin } = require('../util/admin')
 const firebase = require('firebase')
 const config = require('../config')
 const { validatesSignUp, validatesSignIn } = require('../util/validators')
@@ -6,6 +6,7 @@ const { validatesSignUp, validatesSignIn } = require('../util/validators')
 firebase.initializeApp(config)
 
 exports.signUp = (req, res) => {
+
     const newUser = {
         email: req.body.email,
         password: req.body.password,
@@ -18,6 +19,8 @@ exports.signUp = (req, res) => {
     if (Object.keys(errors).length > 0)
         return res.status(400).json(errors)
 
+    const noImg = 'no-img.png'
+
     let token, userId
 
     db
@@ -25,42 +28,47 @@ exports.signUp = (req, res) => {
         .get()
         .then(doc => {
             if (doc.exists)
-                return res.status(400).json({ handle: 'this handle is already taken' })
+                return res.status(400).json({handle: 'this handle is already taken'})
 
             return firebase
                 .auth()
                 .createUserWithEmailAndPassword(newUser.email, newUser.password)
-                .then(data => {
-                    userId = data.user.uid
-                    return data.user.getIdToken()
-                })
-                .then(tkn => {
-                    token = tkn
-                    const userCredentials = {
-                        handle: newUser.handle,
-                        email: newUser.email,
-                        createdAt: new Date().toISOString(),
-                        userId: userId
-                    }
-                    db.doc(`/users/${newUser.handle}`).set(userCredentials)
-                })
-                .then(() => {
-                    return res.status(201).json({ token })
-                })
-                .catch(err => {
-                    console.error(err)
+        })
 
-                    if (err.code === 'auth/email-already-in-use') {
-                        return res.status(400).json({email: 'Email is already in use'})
-                    }
+        .then(data => {
+            userId = data.user.uid
+            return data.user.getIdToken()
+        })
 
-                    if (err.code === 'auth/weak-password') {
-                        return res.status(400).json({email: 'Password should contain at least 6 characters'})
-                    }
+        .then(tkn => {
+            token = tkn
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`,
+                userId: userId
+            }
 
-                    return res.status(500).json({error: err.code})
-                })
+            return db.doc(`/users/${newUser.handle}`).set(userCredentials)
+        })
 
+        .then(() => {
+            return res.status(201).json({ token })
+        })
+
+        .catch(err => {
+            console.error(err)
+
+            if (err.code === 'auth/email-already-in-use') {
+                return res.status(400).json({email: 'Email is already in use'})
+            }
+
+            if (err.code === 'auth/weak-password') {
+                return res.status(400).json({email: 'Password should contain at least 6 characters'})
+            }
+
+            return res.status(500).json({error: err.code})
         })
 }
 
@@ -94,4 +102,57 @@ exports.login = (req, res) => {
             return res.status(500).json({error: err.code})
         })
 
+}
+
+exports.uploadImage = (req, res) => {
+    const BusBoy = require('busboy')
+    const path = require('path')
+    const os = require('os')
+    const fs = require('fs')
+
+    const busboy = new BusBoy({ headers: req.headers })
+
+    let imageFileName
+    let imageToBeUploaded = {}
+
+    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+
+        if (mimetype !== 'image/jpeg' && mimetype !== 'image/jpg' && mimetype !== 'image/png')
+            return res.status(400).json({ error: 'Wrong file type'})
+
+        let imageNameArray = filename.split('.');
+        const imageExtension = imageNameArray[imageNameArray.length - 1]
+        imageFileName = `${Math.round(Math.random() * 1000000)}.${imageExtension}`
+        const filepath = path.join(os.tmpdir(), imageFileName)
+        imageToBeUploaded = { filepath, mimetype }
+        file.pipe(fs.createWriteStream(filepath))
+    })
+
+    busboy.on('finish', () => {
+        admin
+            .storage()
+            .bucket()
+            .upload(imageToBeUploaded.filepath, {
+            resumable: false,
+            metadata: {
+                metadata: {
+                    contentType: imageToBeUploaded.mimetype
+                }
+            }
+        })
+            .then(() => {
+            const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${
+                config.storageBucket
+            }/o/${imageFileName}?alt=media`
+            return db.doc(`/users/${req.user.handle}`).update({ imageUrl })
+        })
+            .then(() => {
+                return res.json({ message: 'Image uploaded successfully' })
+            })
+            .catch(err => {
+                console.log(err)
+                return res.status(500).json({ error: err.code })
+            })
+    })
+    busboy.end(req.rawBody)
 }
--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions');
 const app = require('express')()
 
 const { getAllScreams, newScream } = require('./handlers/screams')
-const { signUp, login } = require('./handlers/users')
+const {signUp, login, uploadImage } = require('./handlers/users')
 const FBAuth = require('./util/FBAuth');
 
 app.get('/screams', getAllScreams)
@@ -10,5 +10,6 @@ app.post('/scream', FBAuth, newScream)
 
 app.post('/signup', signUp)
 app.post('/login', login)
+app.post('/user/image', FBAuth, uploadImage)
 
 exports.api = functions.https.onRequest(app)
