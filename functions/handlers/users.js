e107b914136819492b15c9ef6d7f5bf0b041e9da
keithchasen <keithchasen89@gmail.com>
2019-04-25 16:33:51 +0300

    save user info

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -1,7 +1,7 @@
 const { db, admin } = require('../util/admin')
 const firebase = require('firebase')
 const config = require('../config')
-const { validatesSignUp, validatesSignIn } = require('../util/validators')
+const { validatesSignUp, validatesSignIn, reduceUserDetails } = require('../util/validators')
 
 firebase.initializeApp(config)
 
@@ -102,6 +102,21 @@ exports.login = (req, res) => {
             return res.status(500).json({error: err.code})
         })
 
+}
+
+exports.addUserDetails = (req, res) => {
+    let userDetails = reduceUserDetails(req.body)
+
+    db
+        .doc(`/users/${req.user.handle}`)
+        .update(userDetails)
+        .then(() => {
+            return res.json({ message: 'Details added successfully'})
+        })
+        .catch(err => {
+            console.log(err)
+            return res.status(500).json({error: err.code})
+        })
 }
 
 exports.uploadImage = (req, res) => {
--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions');
 const app = require('express')()
 
 const { getAllScreams, newScream } = require('./handlers/screams')
-const {signUp, login, uploadImage } = require('./handlers/users')
+const {signUp, login, uploadImage, addUserDetails } = require('./handlers/users')
 const FBAuth = require('./util/FBAuth');
 
 app.get('/screams', getAllScreams)
@@ -11,5 +11,6 @@ app.post('/scream', FBAuth, newScream)
 app.post('/signup', signUp)
 app.post('/login', login)
 app.post('/user/image', FBAuth, uploadImage)
+app.post('/user', FBAuth, addUserDetails)
 
 exports.api = functions.https.onRequest(app)
