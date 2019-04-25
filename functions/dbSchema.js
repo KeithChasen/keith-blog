500f2ebd7fcece38dd70b3762d636ee61fdd2181
keithchasen <keithchasen89@gmail.com>
2019-04-25 12:39:30 +0300

    updated db schema / added users

--- functions/dbSchema.js
+++ functions/dbSchema.js
@@ -1,4 +1,16 @@
 let db = {
+    users: [
+        {
+            userId: 'asd67a6a7b677as6d76as7d',
+            email: 'user@mail.com',
+            handle: 'user',
+            createdAt: '2019-04-25T12:29:43.784Z',
+            imageUrl: 'image/asdagdfger/asdasdasdasdas',
+            bio: 'My name is username',
+            website: 'https://site.com',
+            location: 'Kharkiv, UA'
+        }
+    ],
     screams: [
         {
             userHandle: 'user',
